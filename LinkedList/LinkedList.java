371f62d72899420f1a4b5bfd04a43dab275289dd
DeepakLabade <deepaklabade001@gmail.com>
2025-05-27 22:18:38 +0530

    insertion and deletion

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -19,7 +19,7 @@ class LinkedList {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
-        System.out.print("null");
+        System.out.println("null");
     }
 
     void add(int idx, int data) {
@@ -29,11 +29,57 @@ class LinkedList {
             return;
         }
         Node temp = head;
-        for (int i = 0; i < idx-1; i++) {
+        for (int i = 0; i < idx - 1; i++) {
             temp = temp.next;
         }
         newNode.next = temp.next;
         temp.next = newNode;
+    }
+
+    // you {can also}/should make a static variable int size to store the size
+
+    int size() {
+        Node temp = head;
+        int cnt = 0;
+        while (temp != null) {
+            temp = temp.next;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public void removeFirst() {
+        head = head.next;
+    }
+
+    // public boolean rcrSearch(Node node, int target) {
+    //     Node temp = node;
+    //     if (temp != null) {
+    //         return scrSearch(temp, target);
+    //     }
+    //     return false;
+    // }
+
+    void removeMid(int target) {
+        Node prev = head;
+        for (int i = 1; i < target - 1; i++) {
+            prev = prev.next;
+        }
+        Node temp = prev.next;
+        prev.next = temp.next;
+    }
+
+    void reverseLL() {
+        Node curr = head;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
     }
 
     public static void main(String[] args) {
@@ -43,6 +89,9 @@ class LinkedList {
         list.append(2);
         list.add(2, 6);
         list.print();
+        list.removeMid(3);
+        list.print();
+
     }
 }
 
@@ -51,7 +100,7 @@ class Node {
     Node next;
 
     Node(int data) {
-        this.data = data;
+        this.data = data; 
         this.next = null;
     }
 }
