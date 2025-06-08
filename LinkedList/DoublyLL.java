0d1929ff8088e8e2c8f2c986f5315be4839f217d
DeepakLabade <deepaklabade001@gmail.com>
2025-06-08 15:01:29 +0530

    DLL

--- LinkedList/DoublyLL.java
+++ LinkedList/DoublyLL.java
@@ -12,30 +12,125 @@ public class DoublyLL {
         while (prev.next != null) {
             prev = prev.next;
         }
-            prev.next = temp;
-            temp.next = null;
-            temp.back = prev;
+        prev.next = temp;
+        temp.next = null;
+        temp.back = prev;
+    }
+    
+    void deleteHead() {
+        if (head == null || head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        head = temp.next;
+        temp.next = null;
+        head.back = temp;
+    }
+
+    void deleteTail() {
+        if (head == null || head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next.back = null;
+        temp.next = null;
+    }
+
+    void deleteKth(int k) {
+        if (head == null) {
+            return;
+        }
+        int cnt = 1;
+        Node temp = head;
+        while (cnt != k) {
+            cnt++;
+            temp = temp.next;
+        }
+        if (cnt < k) {
+            return;
+        }
+        temp.back.next = temp.next;
+        temp.next.back = temp.back;
+        temp.back = null;
+        temp.next = null;
+    }
+
+    void addHead(int data) {
+        Node temp = new Node(data);
+        if (head == null) {
+            head = temp;
+        }
+        temp.back = null;
+        temp.next = head;
+        head.back = temp;
+        head = temp;
+    }
+
+    void addTail(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        Node prev = temp.back;
+        prev.next = newNode;
+        newNode.next = temp;
+        temp.back = newNode;
+        newNode.back = prev;
+    }
+
+    void addAtK(int data, int k) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        }
+        int cnt = 1;
+        Node temp = head;
+        while (temp.next != null) {
+            cnt++;
+            temp = temp.next;
+            if (cnt == k)
+                break;
+        }
+        Node prev = temp.back;
+        prev.next = newNode;
+        newNode.next = temp;
+        temp.back = newNode;
+        newNode.back = prev;
     }
     
     void print() {
         Node temp = head;
         if (temp == null) {
             System.out.print("null");
+            return;
         }
-        System.out.println("started" + temp.data);
         while (temp != null) {
             System.out.print(temp.data + " -> ");
             temp = temp.next;
         }
+        System.out.println("null");
     }
+
     public static void main(String[] args) {
         DoublyLL ll = new DoublyLL();
         ll.add(5);
         ll.add(2);
         ll.add(19);
+        ll.add(16);
+        ll.add(14);
+        ll.addAtK(29, 2);
         ll.print();
-        System.out.println("done");
     }
+
 }
 
 class Node {
